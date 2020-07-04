f96b801eda9ff7beac6c8363228cb90ed895d7fa
Marco Barbarella <s1089564@studenti.univpm.it>
2020-07-04 10:43:28 +0200

    Bo

--- projectOOP/src/main/java/it/univpm/projectOOP/service/FacebookService.java
+++ projectOOP/src/main/java/it/univpm/projectOOP/service/FacebookService.java
@@ -25,19 +25,19 @@ public class FacebookService {
 	
 	
 	public int ParseJson () {
-	JSONObject obj = new JSONObject(jsonString);
-	String pageName = obj.getJSONObject("pageInfo").getString("pageName");
+		JSONObject obj = new JSONObject(jsonString);
+		String pageName = obj.getJSONObject("pageInfo").getString("pageName");
 
-	JSONArray arr = obj.getJSONArray("posts");
-	for (int i = 0; i < arr.length(); i++)
-	{
-	    String post_id = arr.getJSONObject(i).getString("post_id");
-	    
+		JSONArray arr = obj.getJSONArray("posts");
+		for (int i = 0; i < arr.length(); i++)
+		{
+			String post_id = arr.getJSONObject(i).getString("post_id");
+
+		}
+
+
 	}
-	
-	
-	}
-	
+
 	
 
 	public int getFromFacebook (String fburl) {
