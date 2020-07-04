6eae625221ef160883b5a39addd9f73a63097bc2
matteocirilli <s1089562@studenti.univpm.it>
2020-07-04 11:54:07 +0200

    service2

--- projectOOP/src/main/java/it/univpm/projectOOP/service/FacebookService.java
+++ projectOOP/src/main/java/it/univpm/projectOOP/service/FacebookService.java
@@ -24,15 +24,13 @@ public class FacebookService {
 	}
 
 
-	public int ParseJson () {
+	public void ParseJson (String jsonString) {
+		
 		JSONObject obj = new JSONObject(jsonString);
-		String pageName = obj.getJSONObject("pageInfo").getString("pageName");
-
-		JSONArray arr = obj.getJSONArray("posts");
-		for (int i = 0; i < arr.length(); i++)
-		{
-			String post_id = arr.getJSONObject(i).getString("post_id");
-		}
+		JSONObject photos = obj.getJSONObject("photos");
+		JSONArray data = photos.getJSONArray("data");
+		
+		
 
 
 	}
