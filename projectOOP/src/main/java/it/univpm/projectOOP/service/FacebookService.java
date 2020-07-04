e3a2003f6943506038172d6045f45dce9a8c878b
matteocirilli <s1089562@studenti.univpm.it>
2020-07-04 10:42:33 +0200

    service

--- projectOOP/src/main/java/it/univpm/projectOOP/service/FacebookService.java
+++ projectOOP/src/main/java/it/univpm/projectOOP/service/FacebookService.java
@@ -18,61 +18,66 @@ import org.json.*;
 
 public class FacebookService {
 	private ArrayList<FacebookAlbum> myfblist = new ArrayList<FacebookAlbum>();
-	
+
 	public ArrayList<FacebookAlbum> getFacebookAlbums () {
 		return myfblist;
 	}
-	
-	
+
+
 	public int ParseJson () {
-	JSONObject obj = new JSONObject(jsonString);
-	String pageName = obj.getJSONObject("pageInfo").getString("pageName");
+		JSONObject obj = new JSONObject(jsonString);
+		String pageName = obj.getJSONObject("pageInfo").getString("pageName");
 
-	JSONArray arr = obj.getJSONArray("posts");
-	for (int i = 0; i < arr.length(); i++)
-	{
-	    String post_id = arr.getJSONObject(i).getString("post_id");
-	    
-	}
-	
-	
-	}
-	
-	
+		JSONArray arr = obj.getJSONArray("posts");
+		for (int i = 0; i < arr.length(); i++)
+		{
+			String post_id = arr.getJSONObject(i).getString("post_id");
 
-	public int getFromFacebook (String fburl) {
-		
-		
-	int responsecode=0;
-		
+		}
+
+
+	}
+
+
+
+	public String getFromFacebook (String fburl) {
+
+
+
+
 		try {
-		
-			  URL url = new URL(fburl);
-			  HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-			  //useragent
-			  BufferedReader read = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			  String line = read.readLine();
-			  String html = "";
-			  
-			  while(line!=null) {
-			    html += line;
-			    line = read.readLine();
-			    responsecode = connection.getResponseCode();
-			   
-			  }
-			} catch(MalformedURLException ex) {
-			        ex.printStackTrace();
-			} catch(IOException ioex) {
-			        ioex.printStackTrace();
+
+			URL url = new URL(fburl);
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			//useragent
+			BufferedReader read = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			String line = read.readLine();
+			StringBuilder response = new StringBuilder();
+			int responsecode=0;
+
+			while(line!=null) {
+				response.append(line);
+				responsecode = connection.getResponseCode();
+
 			}
+			
 		
-		return responsecode;
-		
-		
-		
-	
 
 
-}
+		} catch(MalformedURLException ex) {
+			ex.printStackTrace();
+		} catch(IOException ioex) {
+			ioex.printStackTrace();
+		}
+
+
+
+
+
+
+
+
+
+	}
 
 }
