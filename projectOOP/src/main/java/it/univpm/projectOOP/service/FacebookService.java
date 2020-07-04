bf84c5e7cb2535b290889c2cc12f61850712f782
matteocirilli <s1089562@studenti.univpm.it>
2020-07-04 12:35:10 +0200

    album e service

--- projectOOP/src/main/java/it/univpm/projectOOP/service/FacebookService.java
+++ projectOOP/src/main/java/it/univpm/projectOOP/service/FacebookService.java
@@ -19,28 +19,42 @@ import org.json.*;
 public class FacebookService {
 	private ArrayList<FacebookAlbum> myfblist = new ArrayList<FacebookAlbum>();
 
+
 	public ArrayList<FacebookAlbum> getFacebookAlbums () {
 		return myfblist;
 	}
 
 
 	public void ParseJson (String jsonString) {
-		
+
+		FacebookAlbum album = new FacebookAlbum();
+		ArrayList<Integer> altezza = album.getHeight();
+		ArrayList<Integer> larghezza = album.getWidth();
+		ArrayList<String> datecreazione = album.getCreated_time();
+
 		JSONObject obj = new JSONObject(jsonString);
 		JSONObject photos = obj.getJSONObject("photos");
 		JSONArray data = photos.getJSONArray("data");
-		
-		
+
+		for(int i=0;i<data.length();i++)
+		{
+			altezza.add(data.getJSONObject(i).getInt("height"));
+			larghezza.add(data.getJSONObject(i).getInt("width"));
+			datecreazione.add(data.getJSONObject(i).getString("created_time"));
+		}
+		myfblist.add(album);
+
+
 
 
 	}
 
 
 	public String getFromFacebook (String fburl) {
-		
+
 		StringBuilder response = new StringBuilder();
 		int responsecode=0;
-		
+
 		try {
 
 			URL url = new URL(fburl);
@@ -60,10 +74,10 @@ public class FacebookService {
 		} catch(IOException ioex) {
 			ioex.printStackTrace();
 		}
-			
-		
+
+
 		return response.toString();	
-		
+
 
 	}
 
