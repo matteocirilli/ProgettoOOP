d73850ea946f26587ad950bf0d92965e84218697
Marco Barbarella <s1089564@studenti.univpm.it>
2020-07-16 10:24:58 +0200

    2

--- projectOOP/src/test/java/it/univpm/projectOOP/TestWrongFiltStatsException.java
+++ projectOOP/src/test/java/it/univpm/projectOOP/TestWrongFiltStatsException.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 /**
  * Classe TestWrongFiltException.
  */
-public class TestWrongFiltException {
+public class TestWrongFiltStatsException {
 
 	/**
 	 * Sets the up.
